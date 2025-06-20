6cdcea049bf6423dacb11621770db2b44ed373af
Mifaki <agustianto.d19@gmail.com>
2025-06-20 20:02:53 +0700

    fix: increase nim valid length

--- src/models/schema/user.ts
+++ src/models/schema/user.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const registerSchema = z.object({
   name: z.string().min(1, "Name is required").max(100, "Name is too long"),
   email: z.string().email("Invalid email format").max(255, "Email is too long"),
-  nim: z.string().min(13, "NIM is required").max(13, "NIM is too long"),
+  nim: z.string().min(15, "NIM is required").max(15, "NIM is too long"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
 export const loginSchema = z.object({
-  nim: z.string().min(13, "NIM is required").max(13, "NIM is too long"),
+  nim: z.string().min(15, "NIM is required").max(15, "NIM is too long"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
