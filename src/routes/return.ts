488c6fdf1dd58ed18d38fbe80863ea18ed8d37ee
Mifaki <agustianto.d19@gmail.com>
2025-06-21 22:59:11 +0700

    fix: remove return endpoint prefix

--- src/routes/return.ts
+++ src/routes/return.ts
@@ -7,16 +7,12 @@ const upload = multer({ storage: multer.memoryStorage() });
 const router = Router();
 
 router.post(
-  "/return",
+  "/",
   requireAuth,
   upload.single("damagedItem"),
   ReturnController.createReturn
 );
 
-router.post(
-  "/return/:id/status",
-  requireAuth,
-  ReturnController.updateReturnStatus
-);
+router.post("/:id/status", requireAuth, ReturnController.updateReturnStatus);
 
 export default router;
